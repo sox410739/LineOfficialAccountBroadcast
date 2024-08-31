d8810983d891ce8ed3400f349d7b8188e99607d0
林欣蓓 <l.y97622@gmail.com>
2024-08-31 21:52:30 +0800

    change: Refactor Flask static folder path for frontend build

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import webbrowser
 import threading
 import os
 
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder='./frontend/dist', static_url_path='')
 
 
 @app.route('/')
