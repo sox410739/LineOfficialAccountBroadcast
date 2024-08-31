2cdce486e528d272f0d6457c80317dafa4c90739
SOX410739 <wed12345976@gmail.com>
2024-08-31 16:14:50 +0800

    init

---
+++ crawler.py
@@ -0,0 +1,116 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+SELENUIM_WAIT_TIME = 10
+    
+def initailize_driver():
+    driverService = Service('./chromedriver.exe')
+    driver = webdriver.Chrome(service=driverService)
+    return driver
+    
+def login(driver: webdriver.Chrome, url: str):
+    LINE_login_button_xpath = '/html/body/div[2]/div/div[3]/div/form/div/input'
+    login_by_qr_button_xpath = '//*[@id="app"]/div/div/div/div/div/div[2]/a'
+    try:
+        driver.get(url)
+        LINE_login_button = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, LINE_login_button_xpath)))
+        LINE_login_button.click()
+        login_by_qr_button = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, login_by_qr_button_xpath)))
+        login_by_qr_button.click()
+        
+        # wait user to login and scan QR code until the page is redirected to the chat page
+        while driver.current_url != f'{url}?status=success':
+            time.sleep(1)
+    
+    except Exception as e:
+        print('[Login failed]\n')
+        raise e
+        
+def get_all_user(driver: webdriver.Chrome):
+    users_container_xpath = '/html/body/div[2]/div/div[1]/div[1]/main/div/div[1]/div/div[2]/div[2]/div'
+    users_elements_css = 'h6[data-v-a7f2b37c]'
+    try:
+        users_container = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, users_container_xpath)))
+        users_elements = WebDriverWait(users_container, SELENUIM_WAIT_TIME).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, users_elements_css)))
+        # get text from elements
+        users = [user.text for user in users_elements]
+        print(users)
+        return users
+        
+    except Exception as e:
+        print('[Get all user failed]\n')
+        raise e
+        
+def send_message(driver: webdriver.Chrome, users: list, message: str):
+    users_container_xpath = '/html/body/div[2]/div/div[1]/div[1]/main/div/div[1]/div/div[2]/div[2]/div'
+    users_elements_css = 'h6[data-v-a7f2b37c]'
+    textarea_xpath = '/html/body/div[2]/div/div[1]/div[1]/main/div/div[2]/div[3]/div[2]/div[2]/textarea-ex'
+    send_button_xpath = '/html/body/div[2]/div/div[1]/div[1]/main/div/div[2]/div[3]/div[2]/div[3]/div[2]/input'
+    try:
+        for user in users:
+            users_container = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, users_container_xpath)))
+            # get specific user element
+            users_elements = WebDriverWait(users_container, SELENUIM_WAIT_TIME).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, users_elements_css)))
+            for user_element in users_elements:
+                if user_element.text == user:
+                    print(f'User {user} found')
+                    user_element.click()
+                    # send message
+                    textarea = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, textarea_xpath)))
+                    textarea.send_keys(message)
+                    send_button = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, send_button_xpath)))
+                    send_button.click()
+                    break
+
+            # raise Exception(f'User {user} not found')    
+        
+    except Exception as e:
+        print('[Send message failed]\n')
+        raise e
+        
+def send_file(driver: webdriver.Chrome, users: list, file_path: str):
+    users_container_xpath = '/html/body/div[2]/div/div[1]/div[1]/main/div/div[1]/div/div[2]/div[2]/div'
+    users_elements_css = 'h6[data-v-a7f2b37c]'
+    file_input_xpath = '/html/body/div[2]/div/div[1]/div[1]/main/div/div[2]/div[3]/div[2]/div[3]/div[1]/input'
+    file_send_button_xpath = '/html/body/div[2]/div/div[3]/div/div/div[3]/button[1]'
+    try:
+        for user in users:
+            users_container = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, users_container_xpath)))
+            # get specific user element
+            users_elements = WebDriverWait(users_container, SELENUIM_WAIT_TIME).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, users_elements_css)))
+            for user_element in users_elements:
+                if user_element.text == user:
+                    print(f'User {user} found')
+                    user_element.click()
+                    # send file
+                    file_input = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, file_input_xpath)))
+                    # reset file input file path
+                    driver.execute_script('arguments[0].value = ""', file_input)
+                    file_input.send_keys(file_path)
+                    file_send_button = WebDriverWait(driver, SELENUIM_WAIT_TIME).until(EC.presence_of_element_located((By.XPATH, file_send_button_xpath)))
+                    file_send_button.click()
+                    time.sleep(1)
+                    break
+
+            # raise Exception(f'User {user} not found')
+        
+    except Exception as e:
+        print('[Send file failed]\n')
+        raise e
+        
+def login_and_send_message(url: str, message: str):
+    driver = initailize_driver()
+    login(driver, url)
+    users = get_all_user(driver)
+    send_message(driver, users, message)
+
+def login_and_send_file(url: str, file_path: str):
+    driver = initailize_driver()
+    login(driver, url)
+    users = get_all_user(driver)
+    send_file(driver, users, file_path)
+    
---
+++ run.py
@@ -0,0 +1,56 @@
+# app.py
+from flask import Flask, jsonify, request
+from crawler import login_and_send_message, login_and_send_file
+import webbrowser
+import threading
+import os
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+
+@app.route('/')
+def serve_frontend():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/crawler/message', methods=['POST'])
+def web_crawler_message():
+    try:
+        url = request.json['url']
+        message = request.json['message']
+        login_and_send_message(url, message)
+        return jsonify({'message': 'Message sent successfully',
+                        'status': 'success'})
+    except Exception as e:
+        return jsonify({'message': str(e), 'status': 'error'})
+
+
+@app.route('/api/crawler/file', methods=['POST'])
+def web_crawler_file():
+    try:
+        url = request.form['url']
+        # get file binary data
+        file = request.files['file']
+        file_path = f'file/{file.filename}'
+        # clear file directory
+        if os.path.exists('file'):
+            for file_name in os.listdir('file'):
+                os.remove(f'file/{file_name}')
+        else:
+            os.makedirs('file')
+        file.save(file_path)
+        # switch to absolute path
+        file_path = os.path.abspath(file_path)
+        login_and_send_file(url, file_path)
+        return jsonify({'message': 'File sent successfully', 'status': 'success'})
+    except Exception as e:
+        return jsonify({'message': str(e), 'status': 'error'})
+
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
+
+if __name__ == '__main__':
+    threading.Timer(1, open_browser).start()
+    app.run(debug=False)
